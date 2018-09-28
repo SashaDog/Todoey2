39dc9943f82b817ca2da20c2c2d629cabd5a2b95
Alex Urrejola <alexurrejola@alexs-macbook-air.local>
2018-09-28 13:06:10 +0100

    Convert to MVC design pattern

---
+++ Todoey2/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey2
+//
+//  Created by Alex Urrejola on 28/09/2018.
+//  Copyright © 2018 Alex Urrejola. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
